32da54a6f6089c40c3759a1fd87eba2249e56320
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2021-05-10 23:37:38 +0300

    Add custom currency box checkbox

--- src/components/CurrencyBox.vue
+++ src/components/CurrencyBox.vue
@@ -1,10 +1,10 @@
 <template>
   <label class="currency-box" :class="isChecked">
-    <span class="currency-box__checkbox-mark" />
+    <span class="currency-box__checkbox-mark">X</span>
     <input
       type="checkbox"
-      name="vehicle1"
-      value="currency"
+      name="checkbox"
+      :checked="isSelected"
       @input="$emit('select')"
     />
     {{ currency }}
@@ -31,10 +31,11 @@ export default Vue.extend({
 <style scoped lang="scss">
 .currency-box {
   border: 1px solid #a3a3a3;
-  padding: 2px 40px 2px 2px;
+  padding: 2px;
   text-transform: uppercase;
   background: #ececec;
   color: #104372;
+  position: relative;
 
   &:hover {
     background: #dcdcdc;
@@ -43,6 +44,30 @@ export default Vue.extend({
 
   &--selected {
     background: #fff;
+  }
+
+  & input[type="checkbox"] {
+    opacity: 0;
+  }
+
+  &__checkbox-mark {
+    position: absolute;
+    height: 16px;
+    width: 16px;
+    border-radius: 5px;
+    border: 1px #a3a3a3 solid;
+    left: 6px;
+    top: 8px;
+
+    color: transparent;
+    line-height: 16px;
+    font-size: 15px;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  &--selected &__checkbox-mark {
+    color: #ff1f1b;
   }
 }
 </style>
