e48892d936ebb80deb1df6620e1e1794cd00281e
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2021-05-10 20:25:49 +0300

    Display currencies and add ability to select them

---
+++ src/components/CurrencyBox.vue
@@ -0,0 +1,48 @@
+<template>
+  <label class="currency-box" :class="isChecked">
+    <span class="currency-box__checkbox-mark" />
+    <input
+      type="checkbox"
+      name="vehicle1"
+      value="currency"
+      @input="$emit('select')"
+    />
+    {{ currency }}
+  </label>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default Vue.extend({
+  name: "CurrencyBox",
+  props: {
+    currency: String,
+    isSelected: Boolean,
+  },
+  computed: {
+    isChecked() {
+      return this.isSelected ? "currency-box--selected" : "";
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.currency-box {
+  border: 1px solid #a3a3a3;
+  padding: 2px 40px 2px 2px;
+  text-transform: uppercase;
+  background: #ececec;
+  color: #104372;
+
+  &:hover {
+    background: #dcdcdc;
+    cursor: pointer;
+  }
+
+  &--selected {
+    background: #fff;
+  }
+}
+</style>
--- src/components/CurrencySelector.vue
+++ src/components/CurrencySelector.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="currency-selector">
-    <div class="currency-selector__selected"></div>
+    <div class="currency-selector__selected">
+      {{ selectedCurrencies }}
+    </div>
     <div class="currency-selector__list">
-      <div v-for="currency in currencies" :key="currency">
-        {{ currency }}
-      </div>
+      <CurrencyBox
+        v-for="currency in currencies"
+        :key="currency"
+        :currency="currency"
+        :isSelected="isSelectedCurrency(currency)"
+        @select="selectCurrency(currency)"
+      />
     </div>
   </div>
 </template>
@@ -12,11 +18,34 @@
 <script lang="ts">
 import { PropType } from "vue";
 import { Vue } from "vue-property-decorator";
+import CurrencyBox from "./CurrencyBox.vue";
+
+const selectedCurrencies: string[] = [];
 
 export default Vue.extend({
   name: "CurrencySelector",
+  components: {
+    CurrencyBox,
+  },
   props: {
     currencies: Array as PropType<Array<string>>,
+  },
+  data: () => ({
+    selectedCurrencies,
+  }),
+  methods: {
+    isSelectedCurrency(currency: string): boolean {
+      return this.selectedCurrencies.includes(currency);
+    },
+    selectCurrency(currency: string): void {
+      const index = this.selectedCurrencies.indexOf(currency, 0);
+
+      if (index > -1) {
+        this.selectedCurrencies.splice(index, 1);
+      } else {
+        this.selectedCurrencies.push(currency);
+      }
+    },
   },
 });
 </script>
@@ -29,5 +58,11 @@ export default Vue.extend({
   box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.15);
   max-width: 800px;
   padding: 15px;
+
+  &__list {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    gap: 10px 5px;
+  }
 }
 </style>
