319a7ae37b4db1afc8e57a4d048032b66446c91d
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2021-05-10 23:37:14 +0300

    Make font bigger globally and remove text centering and color

--- src/App.vue
+++ src/App.vue
@@ -36,12 +36,11 @@ export default Vue.extend({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100vh;
+  font-size: 24px;
 }
 </style>
